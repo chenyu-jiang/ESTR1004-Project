55716debc38c3003f9be286228252a08a0f2e875
Michael Jiang <740402145@qq.com>
2017-02-16 21:44:03 +0800

    Add files via upload

---
+++ ImageProcessing/ImageProcessing/source.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include "qdbmp.h"
+#include <math.h>
+
+//#define DEBUG
+// REGARDS TO http://qdbmp.sourceforge.net/
+
+void RGBtoBW(BMP* bmp);
+void invert(BMP* bmp);
+BMP* zoom(BMP* bmp, int k);
+void reduceLevel(BMP* bmp, int level);
+void BritandCntr(BMP* bmp, int brightness, double contrast);
+
+int main()
+{
+	BMP*    bmp;
+	char filename[] = "C:\\Users\\HP\\Documents\\Visual Studio 2015\\Projects\\ImageProcessing\\Debug\\Lenna.bmp";
+	bmp = BMP_ReadFile(filename);
+	BMP_CHECK_ERROR(stderr, -1);
+	/////////////////////////////////////////////////////////////////////////
+	//Your code in between
+	BritandCntr(bmp, 50,0.8);
+	/////////////////////////////////////////////////////////////////////////
+	/* Save result */
+	BMP_WriteFile(bmp, "myout.bmp");
+	BMP_CHECK_ERROR(stderr, -2);
+	/* Free all memory allocated for the image */
+	BMP_Free(bmp);
+	//BMP_Free(output);
+	return 0;
+}
+
+void RGBtoBW(BMP* bmp)
+{
+	UCHAR   r, g, b;
+	UINT    width, height;
+	UINT    x, y;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	for (x = 0; x < width; x++)
+	{
+		for (y = 0; y < height; y++)
+		{
+			BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
+			BMP_SetPixelRGB(bmp, x, y, 0.3*r + 0.59*g + 0.11*b, 0.3*r + 0.59*g + 0.11*b, 0.3*r + 0.59*g + 0.11*b);
+		}
+	}
+}
+
+void invert(BMP* bmp)
+{
+	UCHAR   r, g, b;
+	UINT    width, height;
+	UINT    x, y;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	for (x = 0; x < width; x++)
+	{
+		for (y = 0; y < height; y++)
+		{
+			BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
+			BMP_SetPixelRGB(bmp, x, y, 255 - r, 255 - g, 255 - b);
+		}
+	}
+}
+
+BMP* zoom(BMP* bmp, int k)
+{
+	UINT    width, height;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	BMP* newim = BMP_Create(width*k, height*k, BMP_GetDepth(bmp));
+	for (UINT iX = 0; iX < width; iX++)
+	{
+		for (UINT iY = 0; iY < height; iY++)
+		{
+			UCHAR difr, difg, difb;
+			UCHAR tmpr, tmpg, tmpb;
+			BMP_GetPixelRGB(bmp, iX, iY, &tmpr, &tmpg, &tmpb);
+			BMP_GetPixelRGB(bmp, iX, iY + 1, &difr, &difg, &difb);
+			difr = -(tmpr - difr) / k;
+			difg = -(tmpg - difg) / k;
+			difb = -(tmpb - difb) / k;
+			for (int i = 0; i < k; i++)
+			{
+				BMP_SetPixelRGB(newim, iX*k, iY*k + i, tmpr + difr*i, tmpg + difg*i, tmpb + difb*i);
+			}
+		}
+	}
+	for (UINT iY = 0; iY < height*k; iY++)
+	{
+		for (UINT iX = 0; iX < width; iX++)
+		{
+			UCHAR difr, difg, difb;
+			UCHAR tmpr, tmpg, tmpb;
+			BMP_GetPixelRGB(newim, iX*k, iY, &tmpr, &tmpg, &tmpb);
+			BMP_GetPixelRGB(newim, iX*k + k, iY, &difr, &difg, &difb);
+			difr = -(tmpr - difr) / k;
+			difg = -(tmpg - difg) / k;
+			difb = -(tmpb - difb) / k;
+			for (int i = 0; i < k; i++)
+			{
+				BMP_SetPixelRGB(newim, iX*k + i, iY, tmpr + difr*i, tmpg + difg*i, tmpb + difb*i);
+			}
+		}
+	}
+	return newim;
+}
+
+void reduceLevel(BMP* bmp, int level)
+{
+	UINT    width, height;
+	USHORT depth;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	depth = BMP_GetDepth(bmp);
+	for (UINT i = 0; i < width; i++)
+	{
+		for (UINT j = 0; j < height; j++)
+		{
+			UCHAR r,g,b;
+			BMP_GetPixelRGB(bmp, i, j, &r, &g, &b);
+			BMP_SetPixelRGB(bmp, i, j, (UCHAR)(r /(255/level)/(1.0*level-1)*255), (UCHAR)(g / (255 / level) / (1.0*level - 1) * 255), (UCHAR)(b / (255 / level) / (1.0*level - 1) * 255));
+			BMP_CHECK_ERROR(stdout, -1);
+		}
+	}
+}
+
+void BritandCntr(BMP* bmp, int brightness,double contrast) //Brightness: amount of RGB value you want to add
+//Contrast: | 1 means original | <1 less contrast | >1 more contrast
+{
+	UINT    width, height;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	for (UINT i = 0; i < width; i++)
+	{
+		for (UINT j = 0; j < height; j++)
+		{
+			UCHAR Ur, Ug, Ub;
+			BMP_GetPixelRGB(bmp, i, j, &Ur, &Ug, &Ub);
+			int r, g, b;
+			r = (int)(contrast*(Ur - 128)) + 128 + brightness;
+			g = (int)(contrast*(Ug - 128)) + 128 + brightness;
+			b = (int)(contrast*(Ub - 128)) + 128 + brightness;
+			if (r < 0) r = 0;
+			if (g < 0) g = 0;
+			if (b < 0) b = 0;
+			if (r > 255) r = 255;
+			if (g > 255) g = 255;
+			if (b > 255) b = 255;
+			BMP_SetPixelRGB(bmp, i, j, (UCHAR)r, (UCHAR)g, (UCHAR)b);
+			BMP_CHECK_ERROR(stdout, -1);
+		}
+	}
+}
